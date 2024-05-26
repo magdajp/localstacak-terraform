3daec85d49b4d2a3c2133d0be76ed9c08c9a096b
mjanus-patorskaa <magda_janus@interia.eu>
2024-05-26 19:34:45 +0200

    Introduce "image-a" example

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = ">= 3.0"
     }
   }
 }
 
 resource "aws_s3_bucket" "images-bucket" {
   bucket = var.images-bucket_name
+}
+
+resource "aws_s3_bucket_object" "image-a" {
+  bucket       = aws_s3_bucket.images-bucket.id
+  key          = var.image-a-filename
+  source       = var.image-a-path
+  content_type = var.image-a-file-type
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,23 @@
 variable "images-bucket_name" {
-  description = "A name of S3 Bucket"
+  description = "Name of images bucket"
   type        = string
   default     = "images-bucket"
+}
+
+variable "image-a-filename" {
+  description = "Image-a filename"
+  type        = string
+  default     = "image-a.png"
+}
+
+variable "image-a-path" {
+  description = "Path to image-a"
+  type        = string
+  default     = "path/to/image-a.png"
+}
+
+variable "image-a-file-type" {
+  description = "Image-a file type"
+  type        = string
+  default     = "image/png"
 }
